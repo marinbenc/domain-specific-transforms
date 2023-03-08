Use regression based thresholding, fix normalization issues

data/aorta/aorta_dataset.py
@@ -21,6 +21,9 @@ class AortaDataset(base_dataset.BaseDataset):
   # obtained empirically
   GLOBAL_PIXEL_MEAN = 0.1
 
+  GLOBAL_MIN = -200
+  GLOBAL_MAX = 1000
+
   in_channels = 1
   out_channels = 1
 
@@ -32,6 +35,12 @@ class AortaDataset(base_dataset.BaseDataset):
       A.ShiftScaleRotate(p=0.5, rotate_limit=15, scale_limit=0.15, shift_limit=0.15),
       A.GridDistortion(p=0.5),
     ])
+
+  def get_optimal_threshold(self, scan, mask, th_padding=0):
+    aorta_region = scan[mask > 0]
+    high = np.percentile(aorta_region, 100) + th_padding
+    low = np.percentile(aorta_region, 1) - th_padding
+    return low, high
 
   def get_item_np(self, idx, transform=None):
     current_slice_file = self.file_names[idx]
@@ -48,21 +57,23 @@ class AortaDataset(base_dataset.BaseDataset):
 
     scan = np.load(current_slice_file.replace('label/', 'input/'))
     mask = np.load(current_slice_file)
-    scan = scan.astype(np.float)
 
-    scan[scan < -1000] = -1000
-    scan[scan > 1000] = 1000
+    scan[scan < self.GLOBAL_MIN] = self.GLOBAL_MIN
+    scan[scan > self.GLOBAL_MAX] = self.GLOBAL_MAX
+
+    scan = scan.astype(np.float)
 
     if 'itn' in self.transforms:
       aorta_region = scan[mask > 0]
-      th_padding = 20
+      th_aug = 0.05
 
-      self.WINDOW_MAX = np.percentile(aorta_region, 99) + th_padding
-      self.WINDOW_MIN = np.percentile(aorta_region, 1) - th_padding
+      low, high = self.get_optimal_threshold(scan, mask)
+      self.WINDOW_MAX = high
+      self.WINDOW_MIN = low
 
-      if self.augment and self.mode == 'train':
-        self.WINDOW_MAX += np.random.randint(-th_padding * 2, th_padding * 2)
-        self.WINDOW_MIN += np.random.randint(-th_padding * 2, th_padding * 2)
+      if self.augment and self.mode == 'train' and th_aug > 0:
+        self.WINDOW_MAX += np.random.randint(-high * th_aug, high * th_aug)
+        self.WINDOW_MIN += np.random.randint(-high * th_aug, high * th_aug)
 
       # window input slice
       scan[scan > self.WINDOW_MAX] = self.WINDOW_MIN
@@ -71,9 +82,9 @@ class AortaDataset(base_dataset.BaseDataset):
       # plt.imshow(scan, cmap='gray')
       # plt.show()
       # normalize
-      scan = (scan - self.WINDOW_MIN) / (self.WINDOW_MAX - self.WINDOW_MIN)
+      scan = (scan - self.WINDOW_MIN) / (self.WINDOW_MAX - self.WINDOW_MIN + 1e-8)
     else:
-      scan = (scan + 1000) / 2000
+      scan = (scan - self.GLOBAL_MIN) / (self.GLOBAL_MAX - self.GLOBAL_MIN)
 
     if transform is not None:
       transformed = transform(image=scan, mask=mask)

itn/itn_dataset.py
@@ -38,8 +38,8 @@ class ITNDataset(Dataset):
       A.HorizontalFlip(p=0.5),
       A.GridDistortion(p=0.5),
       A.ShiftScaleRotate(p=0.5, rotate_limit=15, scale_limit=0.15, shift_limit=0.15),
-      A.GaussianBlur(p=0.5),
-      A.RandomBrightnessContrast(p=0.5, brightness_limit=0.1, contrast_limit=0.1, brightness_by_max=False),
+      #A.GaussianBlur(p=0.5),
+      #A.RandomBrightnessContrast(p=0.5, brightness_limit=0.05, contrast_limit=0.05, brightness_by_max=False),
       #A.RandomGamma(p=1, gamma_limit=0.5),
       ToTensorV2()
     ], additional_targets={'image_itn': 'image'})
@@ -50,6 +50,8 @@ class ITNDataset(Dataset):
   def __getitem__(self, idx):
     input, label = self.wrapped_dataset.get_item_np(idx)
     input_itn, _ = self.wrapped_dataset_itn.get_item_np(idx)
+    th_low, th_high = self.wrapped_dataset_itn.get_optimal_threshold(input, label)
+    th = torch.tensor([th_low, th_high], dtype=torch.float)
 
     augmentation = self.get_augmentation()
     transformed = augmentation(image=input, image_itn=input_itn, mask=label)
@@ -59,5 +61,5 @@ class ITNDataset(Dataset):
 
     #utils.show_torch(imgs=[input, input_itn, label])
 
-    return input, input_itn
+    return input, th
     

itn/itn_model.py
@@ -9,18 +9,24 @@ import segmentation_models_pytorch as smp
 
 import utils
 
+# TODO: Rename to threshold model or something like that
 class ITN(nn.Module):
   """
   Image Transformer Network
 
   Attributes:
     loc_net: the localization network
-    output_theta: if `True`, the model output will be `(y, theta)`, otherwise it will be `y`
+    output_theta: if `True`, the model output contain theta in the form of (N, 2) tensor 
+                  where N is the batch size and the 2 values are the low and high thresholds
+    output_img: if `True`, the model output contain the thresholded image. 
+                If both `output_theta` and `output_img` are `True`, the output will be (img, theta).
+    segmentation_mode: if `True`, the model will use a sigmoid activation function on the thresholded image.
   """
-  def __init__(self, loc_net, output_theta = False):
+  def __init__(self, loc_net, output_theta=False, output_img=True, segmentation_mode=False):
       super(ITN, self).__init__()
 
       self.output_theta = output_theta
+      self.output_img = output_img
 
       # Spatial transformer localization-network
       self.loc_net = loc_net
@@ -53,26 +59,27 @@ class ITN(nn.Module):
     th_high = 1 / (1 + torch.exp(-slope * (high - x)))
     return th_low + th_high - 1
 
-  # Spatial transformer network forward function
-  def stn(self, x):
-      x_original = x.detach().clone()
-      xs = self.loc_net(x)[-1]
-      
-      xs = self.avg_pool(xs)
-      xs = xs.view(-1, 512 * 4 * 4)
-      theta = self.loc_head(xs)
-
-      th_low = theta[:, 0].view(-1, 1, 1, 1).clip(0, 1)
-      th_high = theta[:, 1].view(-1, 1, 1, 1).clip(0, 1)
-      x = x * self.smooth_threshold(x, th_low, th_high)
-      #x = (x - x.min()) / (x.max() - x.min() + 1e-8)
-      #plt.imshow(x[0, 0].detach().cpu().numpy())
-      #plt.show()
-      return x, theta
-
   def forward(self, x):
-      x, theta = self.stn(x)
-      if self.output_theta:
-        return (x, theta)
-      else:
-        return x
+    assert self.output_img or self.output_theta, "Must output at least one of image or theta"
+    
+    x_original = x.detach().clone()
+    xs = self.loc_net(x)[-1]
+    
+    xs = self.avg_pool(xs)
+    xs = xs.view(-1, 512 * 4 * 4)
+    theta = self.loc_head(xs)
+      
+    if self.output_img:
+      th_low = theta[:, 0].view(-1, 1, 1, 1)
+      th_high = theta[:, 1].view(-1, 1, 1, 1)
+      x = x * self.smooth_threshold(x, th_low, th_high)
+
+      if self.segmentation_mode:
+        x = torch.sigmoid(x)
+
+    if self.output_image and self.output_theta:
+      return x, theta
+    elif self.output_image:
+      return x
+    elif self.output_theta:
+      return theta

itn/train_itn.py
@@ -56,6 +56,7 @@ def train_itn(batch_size, epochs, lr, dataset, subset, log_name):
     valid_loader = DataLoader(valid_dataset, worker_init_fn=worker_init)
     
     model = get_model(train_dataset)
+    model.output_img = False
 
     # TODO: Check if you should pretrain with STN or SEG?
     seg_path = p.join(log_dir, '../seg', 'seg_best.pth')
@@ -75,11 +76,15 @@ def train_itn(batch_size, epochs, lr, dataset, subset, log_name):
 
     writer = SummaryWriter(log_dir=log_dir)
 
-    #loss = torch.nn.MSELoss()
-    loss = torch.nn.L1Loss()
+    l1 = nn.L1Loss()
+    mse = nn.MSELoss()
+
+    def calculate_loss(output, target):
+        loss = mse(output, target)
+        return loss
 
     for epoch in range(1, epochs + 1):
-        utils.train(model, loss, optimizer, epoch, train_loader, valid_loader, writer=writer, checkpoint_name='itn_best.pth', scheduler=None)
+        utils.train(model, calculate_loss, optimizer, epoch, train_loader, valid_loader, writer=writer, checkpoint_name='itn_best.pth', scheduler=scheduler)
     
     writer.close()
 

model.py
@@ -24,6 +24,8 @@ class TransformedSegmentation(nn.Module):
 
       # Spatial transformer localization-network
       self.itn = itn
+      self.itn.output_img = True
+      self.itn.output_theta = False
       self.stn = stn
       self.seg = seg
       self._iters = 0
@@ -33,7 +35,7 @@ class TransformedSegmentation(nn.Module):
     # transform image    
     if self.itn is not None:
       #plt.imshow(x[0].squeeze().detach().cpu().numpy()); plt.show()
-      x = self.itn(x)
+      x_t, th = self.itn(x)
       #plt.imshow(x[0].squeeze().detach().cpu().numpy()); plt.show()
       # normalize x
       #x = (x - x.min()) / (x.max() - x.min())
@@ -51,9 +53,9 @@ class TransformedSegmentation(nn.Module):
       grid = F.affine_grid(theta_inv, y_t.size())
       y = F.grid_sample(y_t, grid)
     else:
-      y = self.seg(x)
+      y = self.seg(x_t)
 
-    #utils.show_torch(imgs=[x[0] + 0.5, x_t[0] + 0.5, y_t[0], y[0]])
+    #utils.show_torch(imgs=[x[0], x_t[0], y[0]])
 
     # self._iters += 1
 

utils.py
@@ -119,6 +119,13 @@ def save_checkpoint(name, log_dir, model, epoch, optimizer, loss):
         'loss': loss
     }, file_name)
 
+def to_device(data, device):
+  if isinstance(data, (list, tuple)):
+    if len(data) == 1:
+      return data[0].to(device, non_blocking=True)
+    return [to_device(x, device) for x in data]
+  return data.to(device, non_blocking=True)
+    
 def train(model, loss_fn, optimizer, epoch, train_loader, val_loader, writer, checkpoint_name, scheduler=None):
     global best_loss
     if epoch == 0:
@@ -126,15 +133,19 @@ def train(model, loss_fn, optimizer, epoch, train_loader, val_loader, writer, ch
     
     model.train()
     loss_total = 0
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        #show_torch(imgs=[data[0] + 0.5, target[0]])
-        optimizer.zero_grad()
-        output = model(data)
-        loss = loss_fn(output, target)
-        loss.backward()
-        optimizer.step()
-        loss_total += loss.item()
+    for batch_idx, batch in enumerate(train_loader):
+      data = batch[0]
+      target = batch[1:]
+      data = to_device(data, device)
+      target = to_device(target, device)
+
+      #show_torch(imgs=[data[0] + 0.5, target[0]])
+      optimizer.zero_grad()
+      output = model(data)
+      loss = loss_fn(output, target)
+      loss.backward()
+      optimizer.step()
+      loss_total += loss.item()
         
     loss_total /= len(train_loader)
     writer.add_scalar('Loss/train', loss_total, epoch)
@@ -145,8 +156,11 @@ def train(model, loss_fn, optimizer, epoch, train_loader, val_loader, writer, ch
       loss_total = 0
       model.eval()
       with torch.no_grad():
-        for (data, target) in val_loader:
-          data, target = data.to(device), target.to(device)
+        for batch in val_loader:
+          data = batch[0]
+          target = batch[1:]
+          data = to_device(data, device)
+          target = to_device(target, device)
           output = model(data)
           loss = loss_fn(output, target)
           loss_total += loss.item()
@@ -164,8 +178,8 @@ def train(model, loss_fn, optimizer, epoch, train_loader, val_loader, writer, ch
         best_loss = loss_total
         save_checkpoint(checkpoint_name, writer.log_dir, model, epoch, optimizer, loss_total)
 
-    #if (epoch - 1) % 25 == 0:
-    #  show_torch(imgs=[data[0], output[0], target[0]])
+    # if (epoch - 1) % 10 == 0:
+    #   show_torch(imgs=[data[0][0], output[0][0], target[0][0]])
 
 def _thresh(img):
   img[img > 0.5] = 1